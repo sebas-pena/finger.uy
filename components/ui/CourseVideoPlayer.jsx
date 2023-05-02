d00a5720a8919e12d312a8f89368722d9fc8b58d
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-05-02 00:06:54 -0300

    Feat: Display youtube classes

--- components/ui/CourseVideoPlayer.jsx
+++ components/ui/CourseVideoPlayer.jsx
@@ -7,10 +7,15 @@ import ClassList from '../list/ClassList'
 const CourseVideoPlayer = ({ classes, courseData, edition }) => {
   const searchParams = useSearchParams()
   const selectedClass = searchParams.get('class') || 0
+  const classData = classes[selectedClass]
   return (
     <main className="flex-1 flex flex-col md:flex-row md:justify-center m-auto w-full md:max-w-7xl">
       <div className="flex-1 md:max-h-[calc(100vh_-_64px)]">
-        <VideoPlayer src={classes[selectedClass].videos} />
+        {
+          classData.videos == null
+            ? <iframe width="420" height="315" className="w-full h-full aspect-video" src={classData.externalLink} />
+            : <VideoPlayer src={classData.videos[0]} />
+        }
       </div>
       <aside className="md:max-w-xs flex flex-col md:max-h-[calc(100vh_-_64px)] md:overflow-y-auto">
         <h3 className="text-center p-2 text-md font-coolvetica font-semibold">{courseData.name}</h3>
